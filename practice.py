ad5834b1322c9ae55c82d6940075c1a145582a75
Milda Barskiete <stanislovaityte.m@gmail.com>
2023-06-06 15:32:05 +0300

    added check_question_activity function

--- practice.py
+++ practice.py
@@ -2,7 +2,18 @@ import csv
 import difflib
 import pandas as pd
 
-# 1. ask questions from csv file
+
+def check_question_activity(filename):
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        questions = []
+        for row in reader:
+            question_id = row[0]
+            activity = row[3]
+            if activity == "True":
+                questions.append(question_id)
+        return questions
+
 
 class Quiz:
     def __init__(self, filename):
@@ -10,27 +21,29 @@ class Quiz:
         self.question_counts = {}
 
     def run_quiz(self):
-        self._load_question_counts()
+        active_questions = self._get_active_questions()
 
         with open(self.filename, "r") as file:
             reader = csv.reader(file)
             rows = list(reader)
             header = rows[0]
             for row in rows[1:]:
-                if row[0].startswith('FFT'):
-                    self._run_fft_question(row)
-                else:
-                    self._run_multiple_choice_question(row)
+                question_id = row[0]
+                if question_id in active_questions:
+                    if row[0].startswith("FFT"):
+                        self._run_fft_question(row)
+                    else:
+                        self._run_multiple_choice_question(row)
 
-        # Update the question counts in the CSV rows
-        for row in rows[1:]:
-            question = row[1]
-            count = self.get_question_count(question)
-            row[4] = str(count)
+            # Update the question counts in the CSV rows
+            for row in rows[1:]:
+                question = row[1]
+                count = self.get_question_count(question)
+                row[4] = str(count)
 
-        with open(self.filename, "w", newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(rows)
+            with open(self.filename, "w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerows(rows)
 
     def _run_fft_question(self, row):
         question = row[1]
@@ -39,23 +52,28 @@ class Quiz:
 
         print(question)
         user_answer = input("YOUR ANSWER: ")
-        if difflib.SequenceMatcher(None, user_answer.lower(), answer.lower()).ratio() >= 0.9:
+        if (
+            difflib.SequenceMatcher(None, user_answer.lower(), answer.lower()).ratio()
+            >= 0.9
+        ):
             print("Correct")
         else:
             print(f"Incorrect. The correct answer is {answer}")
 
     def _run_multiple_choice_question(self, row):
-        question, options = row[1].split(" / ")
+        question_and_options = row[1].split(" / ")
         answer = row[2]
         self._increment_question_count(row[1])
 
-        print(question)
-        print(f'Options: {options}')
-        user_answer = input("YOUR ANSWER: ")
-        if user_answer in answer:
-            print(f"Correct.")
-        else:
-            print(f"Incorrect. The correct answer is {answer}")
+        if len(question_and_options) == 2:
+            question, options = question_and_options
+            print(question)
+            print(f"Options: {options}")
+            user_answer = input("YOUR ANSWER: ")
+            if user_answer in answer:
+                print(f"Correct.")
+            else:
+                print(f"Incorrect. The correct answer is {answer}")
 
     def _increment_question_count(self, question):
         if question in self.question_counts:
@@ -66,29 +84,24 @@ class Quiz:
     def get_question_count(self, question):
         return self.question_counts.get(question, 0)
 
-    def _load_question_counts(self):
-        with open(self.filename, "r") as file:
-            reader = csv.reader(file)
-            next(reader)  # Skip the header row
-            for row in reader:
-                question = row[1]
-                count = int(row[4])
-                self.question_counts[question] = count
+    def _get_active_questions(self):
+        return check_question_activity(self.filename)
 
-# Example usage:
+    def add_question(self, question, answer):
+        with open(self.filename, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["", question, answer, "", "0"])
+
+
 def practice():
-    print("\n" + "You choce to a practice questions. Let's start!")
-    quiz = Quiz("statistics.csv")
-    quiz.run_quiz()
-    
-
-
-# 2. in statistics file create a statistic about answered questions
-# df = pd.read_csv("statistics.csv")
-
-# correctly_answered = df["CORRECTLY ANSWERED Q"].sum()
-# total_questions = len(df)
-
-# 3. from statistic depend how questions will be given
-
-# 4. do not show questions that are disabled
+    questions = check_question_activity("statistics.csv")
+    count_user_questions = len(questions)
+    if count_user_questions < 5:
+        print(
+            f"You need at least 5 questions! You have {count_user_questions} questions"
+        )
+        return
+    else:
+        print("\n" + "You chose to a practice questions. Let's start!")
+        quiz = Quiz("statistics.csv")
+        quiz.run_quiz()
