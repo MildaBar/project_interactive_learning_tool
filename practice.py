db082366fb88ba61f9122b1617e1eab6b787c363
Milda Barskiete <stanislovaityte.m@gmail.com>
2023-06-07 11:20:07 +0300

    questions appear according to weight

--- practice.py
+++ practice.py
@@ -1,6 +1,5 @@
 import csv
 import difflib
-import pandas as pd
 
 
 def check_question_activity(filename):
@@ -19,9 +18,13 @@ class Quiz:
     def __init__(self, filename):
         self.filename = filename
         self.question_counts = {}
+        self.question_weights = {} # create the 'question_weights' attribute as an empty dictionary
 
     def run_quiz(self):
         active_questions = self._get_active_questions()
+        self._load_weights_from_csv()
+
+        question_pool = []
 
         with open(self.filename, "r") as file:
             reader = csv.reader(file)
@@ -30,22 +33,29 @@ class Quiz:
             for row in rows[1:]:
                 question_id = row[0]
                 if question_id in active_questions:
-                    if row[0].startswith("FFT"):
-                        self._run_fft_question(row)
-                    else:
-                        self._run_multiple_choice_question(row)
+                    question_pool.append(row)
 
-            # Update the question counts in the CSV rows
-            for row in rows[1:]:
+            question_pool.sort(key=lambda row: self.question_weights.get(row[0], 1.0))
+
+            for row in question_pool:
+                question_id = row[0]
+                if row[0].startswith("FFT"):
+                    self._run_fft_question(row)
+                else:
+                    self._run_multiple_choice_question(row)
+                # Update the question counts in the CSV rows
                 question = row[1]
                 count = self.get_question_count(question)
                 row[4] = str(count)
 
-            with open(self.filename, "w", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerows(rows)
+        with open(self.filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+
+        self._save_weights_to_csv()
 
     def _run_fft_question(self, row):
+        qu_id = row[0]
         question = row[1]
         answer = row[2]
         self._increment_question_count(question)
@@ -57,10 +67,13 @@ class Quiz:
             >= 0.9
         ):
             print("Correct")
+            self._update_question_weight(qu_id, decrease=True, weight_index=5)
         else:
-            print(f"Incorrect. The correct answer is {answer}")
+            print(f"Incorrect. The correct answer is: '{answer}'")
+            self._update_question_weight(qu_id, decrease=False, weight_index=5)
 
     def _run_multiple_choice_question(self, row):
+        qu_id = row[0]
         question_and_options = row[1].split(" / ")
         answer = row[2]
         self._increment_question_count(row[1])
@@ -72,14 +85,64 @@ class Quiz:
             user_answer = input("YOUR ANSWER: ")
             if user_answer in answer:
                 print(f"Correct.")
+                self._update_question_weight(qu_id, decrease=True, weight_index=5)
             else:
-                print(f"Incorrect. The correct answer is {answer}")
+                print(f"Incorrect. The correct answer is: '{answer}'")
+                self._update_question_weight(qu_id, decrease=False, weight_index=5)
+
+
+    def _load_weights_from_csv(self):
+        with open(self.filename, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                question_id = row[0]
+                weight = float(row[5])
+                self.question_weights[question_id] = weight
+
+    def _update_question_weight(self, qu_id, decrease, weight_index):
+        # Update the weight in memory
+        weight = self.question_weights[qu_id]
+        if decrease:
+            weight *= 0.9  # Decrease the weight by 10%
+        else:
+            weight *= 1.1  # Increase the weight by 10%
+        self.question_weights[qu_id] = weight
+
+        # format the weight as a float number
+        formatted_weight = round(weight, 2)
+
+        # Update the weight in the CSV file
+        with open(self.filename, "r") as file:
+            rows = list(csv.reader(file))
+            for row in rows:
+                if row[0] == qu_id:
+                    row[weight_index] = formatted_weight
+                    break
+
+        with open(self.filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+
+    def _save_weights_to_csv(self):
+        with open(self.filename, "r") as file:
+            rows = list(csv.reader(file))
+            for row in rows[1:]:
+                question_id = row[0]
+                weight = self.question_weights.get(question_id, 1.0)
+                row[5] = round(weight, 2)
+                
+
+        with open(self.filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
 
     def _increment_question_count(self, question):
         if question in self.question_counts:
             self.question_counts[question] += 1
         else:
             self.question_counts[question] = 1
+
 
     def get_question_count(self, question):
         return self.question_counts.get(question, 0)
