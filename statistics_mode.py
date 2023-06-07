e65f144fe1a70871c2759522086aa21626d06f32
Milda Barskiete <stanislovaityte.m@gmail.com>
2023-06-07 15:15:49 +0300

    clean code

--- statistics_mode.py
+++ statistics_mode.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from tabulate import tabulate
 
+
 class CSVPrinter:
     def __init__(self, filename):
         self.filename = filename
 
     def print_csv_info(self):
         data = pd.read_csv(self.filename)
-        print(tabulate(data, headers='keys', tablefmt='simple'))
+        print(tabulate(data, headers="keys", tablefmt="simple"))
 
-# Usage
+
 def statistics_mode():
-    csv_printer = CSVPrinter('statistics.csv')
+    csv_printer = CSVPrinter("statistics.csv")
     csv_printer.print_csv_info()
